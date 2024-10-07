a80e13da85eefff63974ec26f8795ab96d70cd38
jh <jhoehlein04@gmail.com>
2024-10-07 15:31:59 +0200

    translateNumber gibt nun die deutsche Übersetzung der zahlen 1 bis 10 zurück

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/GermanTranslator.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/GermanTranslator.java
@@ -8,9 +8,11 @@ public class GermanTranslator implements Translator {
 	 * Methode zur Übersetzung einer Zahl in eine String-Repraesentation
 	 */
 	public String translateNumber( int number ) {
-		// [ihr Source Code aus Übung 1-2]
-
-		return "null";
+		if(number > 10 || number < 1) {		// nur Zahlen im bereich zwischen 1 und 10 werden übersetzt sonst return fehlermeldung
+			return "Übersetzung der Zahl " + number + " nicht möglich ([" + version + "])";
+		}
+		String gerNum[] = {"eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn"};
+		return gerNum[number-1];		// übersetzung mittels index im string[]
 	}
 
 	/**
