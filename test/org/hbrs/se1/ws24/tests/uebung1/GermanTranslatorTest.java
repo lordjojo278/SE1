f0f15712d84e42c678aa9743f2c3d8ad8bd861fb
jh <jhoehlein04@gmail.com>
2024-10-07 16:02:43 +0200

    tests hinzugefügt

--- test/org/hbrs/se1/ws24/tests/uebung1/GermanTranslatorTest.java
+++ test/org/hbrs/se1/ws24/tests/uebung1/GermanTranslatorTest.java
@@ -5,11 +5,27 @@ import org.junit.jupiter.api.Test;
 
 public class GermanTranslatorTest {
 
-    @Test
-    public void aTest() {
-        GermanTranslator translator = new GermanTranslator();
+    private Translator translator;
 
-        assertEquals("null" , translator.translateNumber(0));
+    @BeforeEach
+    public void setUp() {
+        translator = new GermanTranslator();
+    }
+
+    // Positivfälle: Gültige Zahlen zwischen 1 und 10
+    @Test
+    public void testValidNumbers() {
+        assertEquals("eins", translator.translateNumber(1));
+        assertEquals("zwei", translator.translateNumber(2));
+        assertEquals("zehn", translator.translateNumber(10));
+    }
+
+    // Negativfälle: Ungültige Zahlen außerhalb des Bereichs
+    @Test
+    public void testInvalidNumbers() {
+        assertEquals("Übersetzung der Zahl 0 nicht möglich (1.0)", translator.translateNumber(0));
+        assertEquals("Übersetzung der Zahl 11 nicht möglich (1.0)", translator.translateNumber(11));
+        assertEquals("Übersetzung der Zahl -5 nicht möglich (1.0)", translator.translateNumber(-5));
     }
 
 }
