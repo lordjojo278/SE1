2a869c0cae6ee31335b30ebb412493448f0ec59d
jh <jhoehlein04@gmail.com>
2024-10-21 15:56:53 +0200

    container new

--- src/org/hbrs/se1/ws24/exercises/uebung2/Container.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/Container.java
@@ -3,39 +3,94 @@ package org.hbrs.se1.ws24.exercises.uebung2;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hbrs.se1.ws24.exercises.uebung3.persistence.PersistenceException;
+import org.hbrs.se1.ws24.exercises.uebung3.persistence.PersistenceStrategy;
 public class Container {
-    private List<Member> members = new ArrayList<>();
+    // Singleton-Instanz
+    private static Container instance;
 
-    // Methode zum Hinzufügen eines Members
+    // Interne Liste zur Abspeicherung der Member-Objekte
+    private List<Member> liste = new ArrayList<>();
+
+    // Persistenzstrategie
+    private PersistenceStrategy<Member> strategy;
+
+    // Privater Konstruktor (für das Singleton)
+    private Container() {}
+
+    // Statische Methode zur Rückgabe der einzigen Instanz
+    public static Container getInstance() {
+        if (instance == null) {
+            instance = new Container();
+        }
+        return instance;
+    }
+
+    // Methode zum Setzen der Persistenzstrategie
+    public void setPersistenceStrategy(PersistenceStrategy<Member> strategy) {
+        this.strategy = strategy;
+    }
+
+    // Methode zum Speichern der Member-Objekte
+    public void store() throws PersistenceException {
+        if (strategy == null) {
+            throw new PersistenceException(PersistenceException.ExceptionType.NoStrategyIsSet, "Keine Persistenzstrategie gesetzt!");
+        }
+        strategy.save(liste);
+    }
+
+    // Methode zum Laden der Member-Objekte
+    public void load() throws PersistenceException {
+        if (strategy == null) {
+            throw new PersistenceException(PersistenceException.ExceptionType.NoStrategyIsSet, "Keine Persistenzstrategie gesetzt!");
+        }
+        liste = strategy.load();
+    }
+
+    // Methode zum Hinzufügen von Member-Objekten
     public void addMember(Member member) throws ContainerException {
-        for (Member m : members) {
+        if (member == null) {
+            throw new ContainerException("Member-Objekt ist null");
+        }
+        if (contains(member)) {
+            throw new ContainerException("Member-Objekt mit der ID " + member.getID() + " ist bereits vorhanden!");
+        }
+        liste.add(member);
+    }
+
+    // Methode zur Überprüfung, ob ein Member-Objekt bereits vorhanden ist
+    private boolean contains(Member member) {
+        for (Member m : liste) {
             if (m.getID().equals(member.getID())) {
-                throw new ContainerException("Das Member-Objekt mit der ID " + member.getID() + " ist bereits vorhanden!");
+                return true;
             }
         }
-        members.add(member);
+        return false;
     }
 
-    // Methode zum Löschen eines Members anhand der ID
+    // Methode zum Löschen eines Member-Objekts
     public String deleteMember(Integer id) {
-        for (Member m : members) {
+        Member member = getMember(id);
+        if (member != null) {
+            liste.remove(member);
+            return "Member mit der ID " + id + " wurde gelöscht.";
+        } else {
+            return "Member mit der ID " + id + " nicht gefunden.";
+        }
+    }
+
+    // Methode zum Abrufen der aktuellen Liste von Member-Objekten
+    public List<Member> getCurrentList() {
+        return new ArrayList<>(liste);
+    }
+
+    // Interne Methode zur Ermittlung eines Member-Objekts
+    private Member getMember(Integer id) {
+        for (Member m : liste) {
             if (m.getID().equals(id)) {
-                members.remove(m);
-                return "Member mit der ID " + id + " erfolgreich gelöscht.";
+                return m;
             }
         }
-        return "Kein Member mit der ID " + id + " gefunden.";
-    }
-
-    // Methode zur Ausgabe aller Member-IDs
-    public void dump() {
-        for (Member m : members) {
-            System.out.println(m.toString());
-        }
-    }
-
-    // Methode zur Ermittlung der Anzahl gespeicherter Member
-    public int size() {
-        return members.size();
+        return null;
     }
 }
