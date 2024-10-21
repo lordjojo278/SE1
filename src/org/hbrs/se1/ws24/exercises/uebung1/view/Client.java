d573b614d83ec7385ba0415542b01299f3a57e05
jh <jhoehlein04@gmail.com>
2024-10-21 14:29:23 +0200

    commit this

--- src/org/hbrs/se1/ws24/exercises/uebung1/view/Client.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/view/Client.java
@@ -1,33 +1,33 @@
 package org.hbrs.se1.ws24.exercises.uebung1.view;
 
-import com.sun.java.accessibility.util.Translator;
 import org.hbrs.se1.ws24.exercises.uebung1.control.Factory;
+import org.hbrs.se1.ws24.solutions.uebung1.control.Translator;
+import org.hbrs.se1.ws24.solutions.uebung1.control.factory.TranslatorFactory;
 
 public class Client{
 
-	private Translator translator;
+	/**
+	 * Methode zur Ausgabe einer Zahl auf der Console
+	 *
+	 */
 
-	// Konstruktor, der ein Translator-Objekt über die Factory zuweist
-	public Client() {
-		this.translator = Factory.createTranslator();
+	public void display( int aNumber ){
+		// In dieser Methode soll die Methode translateNumber
+		// mit dem übergegebenen Wert der Variable aNumber
+		// aufgerufen werden.
+		// Strenge Implementierung gegen das Interface Translator gewuenscht!
+
+		// Referenz aktiv beziehen
+		Translator translator = TranslatorFactory.createGermanTranslator();
+		String result = translator.translateNumber( aNumber );
+
+		System.out.println("Das Ergebnis der Berechnung: " + result );
+
+		translator = TranslatorFactory.createEnglishTranslator();
+		result = translator.translateNumber( aNumber );
+		System.out.println("Das Ergebnis der Berechnung: " + result );
+
 	}
-
-		/**
-		 * Methode zur Ausgabe einer Zahl auf der Console
-		 * (auch bezeichnet als CLI, Terminal)
-		 *
-		 */
-		 void display( int aNumber ){
-			// In dieser Methode soll die Methode translateNumber
-			// mit dem übergegebenen Wert der Variable aNumber
-			// aufgerufen werden.
-			//
-			// Strenge Implementierung (nur) gegen das Interface Translator gewuenscht!
-
-			 String result = translator.translateNumber(aNumber);
-			 System.out.println("Das Ergebnis der Berechnung: " + result);
-
-		 }
 }
 
 
